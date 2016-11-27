29e4f5b8e937ea24cfdaa23ced80b40f7bf7734e
guiand <guillaume.androz@icentia.com>
2016-11-26 21:02:55 -0500

    Add response to the POST. A message is then displayed on Slack channel

--- slack_webhook.py
+++ slack_webhook.py
@@ -1,5 +1,6 @@
 import os
 from flask import Flask, request, Response
+import json
 
 
 app = Flask(__name__)
@@ -15,9 +16,20 @@ def toto():
         text = request.form.get('text')
         inbound_message = '{} in {} says {}'.format(username, channel, text)
         print(inbound_message)
-    return Response(), 200
+        js = json.dumps({"text": "Coucou depuis le webhook!"})
+        resp = Response(js, status=200, mimetype='application/json')
+    else:
+        js = json.dumps({"text": "Bad request!"})
+        resp = Response(js, status=200, mimetype='application/json')
+    return resp
 
 
 if __name__ == "__main__":
     # Use ngrok for easy and secure tunneling to localhost
+    # First launch ngrok with the command:
+    # > ngrok http 5000
+    # This command will initiate a tunneling to localhost:5000
+    # Set the Slack application webhook URL to the given ngrok URL
+    # Set the SLACK_WEBHOOK_SECRET environment variable in the config of the python script on pycharm
+    # Enjoy!
     app.run(debug=True)
